97f4a83581a7a73b1b3e375b99c04f25c02f4cb7
Ahaanjitsingh77 <Ahaanjit.singh77@gmail.com>
2025-10-16 21:20:08 -0400

    name

--- src/Oct11/Rev.java
+++ src/Oct11/Rev.java
@@ -3,10 +3,13 @@ package Oct11;
 public class Rev {
     public static void main(String[] args) {
         String s1 = "Hello I am learning java";
-        String[] s = s1.split(" ");
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            s1.charAt(i);
-            System.out.print(s1.charAt(i));
+        String[] words = s1.split(" ");
+
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            StringBuilder stringBuilder = new StringBuilder(word);
+            result.append(stringBuilder.reverse() + " ");
         }
+        System.out.println(result);
     }
 }
---
+++ src/Oct14/Namee.java
@@ -0,0 +1,7 @@
+package Oct14;
+
+public class Namee {
+    public static void main(String[] args) {
+        System.out.println("My name is Ahaanjit Singh");
+    }
+}
